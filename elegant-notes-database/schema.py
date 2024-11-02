227caae267269b4c8eb51e6b94df73ecc9024cb1
Seth Gunkel <45577605+sgunkel@users.noreply.github.com>
2024-11-01 23:03:00 -0500

    Add Page/Block schema

--- elegant-notes-database/schema.py
+++ elegant-notes-database/schema.py
@@ -8,107 +8,34 @@
 #   the existing database schema
 ####
 """
-Title: This is an Example
-Description: Example to show how schema works
-Authors: TerminusDB, Cheuk
+Title: Elegant Notes Storage
+Description: Storage to be used by the Elegant Notes application
+Authors: Seth Gunkel
 """
-from typing import List, Optional, Set
+from typing import Set
 
-from terminusdb_client.woqlschema import (
-    DocumentTemplate,
-    EnumTemplate,
-    HashKey,
-    RandomKey,
-    TaggedUnion,
-)
+from terminusdb_client.woqlschema import DocumentTemplate
 
-
-class Country(DocumentTemplate):
-    """This is Country.
-    Country is a class object in the schema. It's class attributes will be the properties of the object. Therefore, a Country object will have a name which is string and a list of alias names that is called 'also_know_as'
-    """
-
-    _key = HashKey(["name"])
-    also_know_as: List[str]
-    name: str
-
-
-class Address(DocumentTemplate):
-    """This is address"""
-
-    _subdocument = []
-    country: "Country"
-    postal_code: str
-    street: str
-
-
-class Coordinate(DocumentTemplate):
-    _key = RandomKey()
-    _abstract = []
-    x: float
-    y: float
-
-
-class Team(EnumTemplate):
-    Information_Technology = "Information Technology"
-    Marketing = ()
-
-
-class Contact(TaggedUnion):
-    """TaggedUnion allow options for types"""
-
-    _key = RandomKey()
-    international: str
-    local_number: int
-
-
-class Person(DocumentTemplate):
-    """This is a person
-    Can store the explanation to the attributes in the docstring. Docstrings needs to be in numpydoc format.
+class Block(DocumentTemplate):
+    """The base object that holds all user-entered information in the application
 
         Attributes
         ----------
-        age : int
-            Age of the person.
-        name : str
-            Name of the person.
+        text : str
+            User entered text, optionally in Markdown format.
+        children : iterable
+            Nested Block objects belonging to this parent object.
     """
+    text: str
+    children: Set["Block"]
 
-    _key = RandomKey()
-    age: int
-    friend_of: Set["Person"]
-    name: str
+class Page(DocumentTemplate):
+    """Root/high-level object with children objects for user-entered text
 
-
-class Employee(Person):
-    """Employee will inherits the attributes from Person
-
-    Attributes
-    ----------
-    age : int
-        Age of the person.
-    name : str
-        Name of the person.
+        Attributes
+        ----------
+        children : iterable
+            Block objects that make up the structure of the page, providing
+            (optional) Markdown text for extensibility.
     """
-
-    _key = RandomKey()
-    address_of: "Address"
-    age: int
-    contact_number: Optional[str]
-    friend_of: Set["Person"]
-    managed_by: "Employee"
-    name: str
-
-
-class Location(Address, Coordinate):
-    """Location is inherits from Address and Coordinate
-    Class can have multiple inheritance. It will inherits both the attributes from Address and Coordinate.
-    """
-
-    _subdocument = []
-    country: "Country"
-    name: str
-    postal_code: str
-    street: str
-    x: float
-    y: float
+    children: Set[Block]
