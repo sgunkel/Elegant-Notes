be80d1b8e1122dd8128bf564e191ac286e7a0508
Seth Gunkel <45577605+sgunkel@users.noreply.github.com>
2024-11-02 23:35:04 -0500

    Add database structure and basic example

--- elegant-notes-backend/main.py
+++ elegant-notes-backend/main.py
@@ -1,4 +1,47 @@
-from elegant_notes_database import schema
+from typing import List
 
-if __name__ == '__main__':
-    print('it works')
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from elegant_notes_database.db_maker import make_db
+
+app = FastAPI()
+db = make_db()
+
+class BlockModel(BaseModel):
+    text: str
+    parent_id: str
+    children: List["BlockModel"]
+
+class PageModel(BaseModel):
+    name: str
+    children: List[BlockModel]
+
+@app.get('/pages')
+def get_all_pages():
+    return db.page_api.retrieve_all()
+
+@app.post('/pages/add')
+def add_page(new_page: PageModel):
+    return db.page_api.create(new_page.model_dump())
+
+@app.get('/blocks')
+def get_all_blocks():
+    return db.block_api.retrieve_all()
+
+@app.post('/blocks/add')
+def add_block(new_block: BlockModel):
+    raw_block = new_block.model_dump()
+    parent_id = raw_block.pop('parent_id')
+    created_id = db.block_api.create(raw_block)
+    created = db.block_api.retrieve_by_id(created_id)
+
+    if parent_id.startswith('Page'):
+        page = db.page_api.retrieve_by_id(parent_id)
+        page['children'].append(created)
+        edited = db.page_api.update(page)
+    else:
+        block = db.block_api.retrieve_by_id(parent_id)
+        block['children'].append(created)
+        edited = db.block_api.update(block)
+    return {'created': created, 'edited': edited}
--- elegant-notes-database/elegant_notes_database/schema.py
+++ elegant-notes-database/elegant_notes_database/schema.py
@@ -1,41 +1,50 @@
 ####
 # This is the script for storing the schema of your TerminusDB
 # database for your project.
-# Use 'tdbpy startproject' to create a new project, and press enter on
-#   endpoint information to use localhost setup
 # Use 'tdbpy commit' to commit changes to the database and
 # use 'tdbpy sync' to change this file according to
-#   the existing database schema
+# the existing database schema
 ####
 """
 Title: Elegant Notes Storage
 Description: Storage to be used by the Elegant Notes application
 Authors: Seth Gunkel
 """
-from typing import Set
+from typing import List
 
-from terminusdb_client.woqlschema import DocumentTemplate
+from terminusdb_client.woqlschema import DocumentTemplate, Schema
+
+_custom_schema = Schema()
+def get_schema():
+    global _custom_schema
+    return _custom_schema
 
 class Block(DocumentTemplate):
     """The base object that holds all user-entered information in the application
 
-        Attributes
-        ----------
-        text : str
-            User entered text, optionally in Markdown format.
-        children : iterable
-            Nested Block objects belonging to this parent object.
+    Attributes
+    ----------
+    children : List['Block']
+        Nested Block objects belonging to this parent object.
+    text : str
+        User entered text, optionally in Markdown format.
     """
+
+    _schema = _custom_schema
+    children: List["Block"]
     text: str
-    children: Set["Block"]
 
 class Page(DocumentTemplate):
     """Root/high-level object with children objects for user-entered text
 
-        Attributes
-        ----------
-        children : iterable
-            Block objects that make up the structure of the page, providing
-            (optional) Markdown text for extensibility.
+    Attributes
+    ----------
+    children : List['Block']
+        Block objects that make up the structure of the page
+    name : str
+        Name of the page
     """
-    children: Set[Block]
+
+    _schema = _custom_schema
+    children: List["Block"]
+    name: str
