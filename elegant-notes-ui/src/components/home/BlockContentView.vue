b282160f335099c802ecfa452a143a2568d26c69
Seth Gunkel <45577605+sgunkel@users.noreply.github.com>
2024-11-24 00:32:37 -0600

    Add edit/presentation states to block views

--- elegant-notes-ui/src/components/home/BlockContentView.vue
+++ elegant-notes-ui/src/components/home/BlockContentView.vue
@@ -1,11 +1,44 @@
 <script>
+import { nextTick, ref } from 'vue';
+
+
 export default {
     props: {
         block: Object,
     },
     data() {
         return {
-            //
+            // state is created and given to us from setup()
+        }
+    },
+    setup() {
+        /*
+         * Vue3 makes this portion a little hacky, but this is how we can set the
+         *   input focused states per component; since this component has several
+         *   instances, it's slightly trickier to grab the correct <input> node.
+         * https://laracasts.com/discuss/channels/vue/how-to-set-focus-on-a-newly-shown-input-element
+        */
+        const focused = ref(false)
+        const textInput = ref(null) // some dark magic will assign this to the <input> component with ref="textInput"...
+        const setFocus = () => {
+            nextTick(() => {
+                textInput.value.focus()
+            })
+        }
+        return {
+            focused,
+            textInput,
+            setFocus
+        }
+    },
+    methods: {
+        enterEditMode() {
+            this.focused = true
+            this.setFocus()
+        },
+        EnterPresentationMode() {
+            this.focused = false
+            // TODO: save changes!
         }
     }
 }
@@ -13,7 +46,19 @@ export default {
 
 <template>
     <ul class="bcv-wrapper">
-        <li>{{ block.text }}</li>
+        <li>
+            <input
+              v-if="focused"
+              type="text"
+              ref="textInput"
+              v-model="block.text"
+              @blur="EnterPresentationMode">
+            <span
+              v-else
+              @click="enterEditMode">
+                {{ block.text }}
+            </span>
+        </li>
         <BlockContentView
           v-for="child in block.children"
           :block="child">
