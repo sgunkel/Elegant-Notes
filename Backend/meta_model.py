7da3ae99b5a5e7f55c9e73648af8eafb67770762
Seth Gunkel <45577605+sgunkel@users.noreply.github.com>
2025-04-08 00:04:04 -0500

    Add child Block retrieval to backlink

--- Backend/meta_model.py
+++ Backend/meta_model.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 class BackLinkReference(BaseModel):
     line: str
     line_number: int
+    children: List[str]
 
 class BackLink(BaseModel):
     page_name: str
--- Backend/meta_utils.py
+++ Backend/meta_utils.py
@@ -3,7 +3,6 @@ import re
 from typing import List
 
 from .config import get_pages_path
-from .page_model import NamedPage
 from .meta_model import BackLink, BackLinkReference
 
 PAGE_PATH = get_pages_path()
@@ -25,22 +24,40 @@ def __extract_references_from_page(path: str, page_name: str) -> List[BackLinkRe
         content = f.readlines()
     
     all_references = []
-    line_number = 0
+    line_number = 1
     for line in content:
-        extracted = __extract_back_links_from_line(page_name, line, line_number)
+        line_offset = line_number - 1
+        rest = content[line_offset:]
+        extracted = __extract_back_links_from_line(page_name, line, line_number, rest)
         all_references.extend(extracted)
         line_number += 1
     return all_references
 
-def __extract_back_links_from_line(page_name: str, line: str, line_number: int) -> List[BackLinkReference]:
+def __extract_back_links_from_line(page_name: str, line: str, line_number: int, remaining_lines: List[str]) -> List[BackLinkReference]:
     matches = re.findall(r"\[\[.*?\]\]", line)
-    return __extract_back_link_from_reference(matches, page_name, line, line_number)
+    return __extract_back_link_from_reference(matches, page_name, line, line_number, remaining_lines)
 
-def __extract_back_link_from_reference(matches: List[str], page_name: str, line: str, line_number: int) -> List[BackLinkReference]:
+def __extract_back_link_from_reference(matches: List[str], page_name: str, line: str, line_number: int, remaining_lines: List[str]) -> List[BackLinkReference]:
     references = []
     for ref_match in matches:
         back_link_name = re.sub(r"(\[\[|\]\])", "", ref_match)
         if back_link_name == page_name:
-            reference = BackLinkReference(line=line, line_number=line_number)
+            children = __extract_block_children(line, remaining_lines)
+            reference = BackLinkReference(line=line, line_number=line_number, children=children)
             references.append(reference)
     return references
+
+# should be generic enough for both Block and Page (back-link) references
+def __extract_block_children(current_line: str, remaining_lines: List[str]) -> List[str]:
+    children = []
+    indention_start = __extract_indention_start(current_line)
+    indention_start_length = len(indention_start)
+    for line in remaining_lines[1:]: # remaining_lines includes current_line, so we just skip it
+        child_indention = __extract_indention_start(line)
+        if len(child_indention) <= indention_start_length:
+            break
+        children.append(line)
+    return children
+
+def __extract_indention_start(line: str) -> str:
+    return re.match(r'^\s*', line).group()
