3640657a274bb5c679e19d48f606594b25e5d67c
Seth Gunkel <45577605+sgunkel@users.noreply.github.com>
2025-07-30 14:32:08 -0500

    Add initial Python meta util tests

--- Backend/config.py
+++ Backend/config.py
@@ -1,13 +1,22 @@
 from pathlib import Path
 import os
 
-DB_PATH = Path.home() / 'elegant-notes-db'
+DEFAULT_DB_PATH = Path.home() / 'elegant-notes-db'
+_db_path = DEFAULT_DB_PATH
 
 def __create_path_if_not_exit(path: Path) -> None:
     if not path.exists():
         os.makedirs(str(path))
 
 def get_pages_path() -> Path:
-    path = DB_PATH / 'pages'
+    path = _db_path / 'pages'
     __create_path_if_not_exit(path)
     return path
+
+def set_db_path(new_path: Path) -> None:
+    global _db_path
+    _db_path = new_path
+
+def reset_db_path() -> None:
+    global _db_path
+    _db_path = DEFAULT_DB_PATH
