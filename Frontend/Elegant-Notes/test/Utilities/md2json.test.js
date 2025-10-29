9869551ca6ee552161c242d450a6ecef156bedc0
Seth Gunkel <45577605+sgunkel@users.noreply.github.com>
2025-10-28 23:10:33 -0500

    Fix test imports to new util file

--- Frontend/Elegant-Notes/test/Utilities/md2json.test.js
+++ Frontend/Elegant-Notes/test/Utilities/md2json.test.js
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest'
 import { removeIDsFromBlockObjects } from '../blockObjUtils.js'
-import { md2json } from '@/helpers/md2json.js'
+import { md2json } from '@/helpers/MarkdownJSONUtils.js'
 
 ///
 /// Mock Markdown content to be parsed
