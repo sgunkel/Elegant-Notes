01f600653378446560485c7be132d2fc6b8f0540
Seth Gunkel <45577605+sgunkel@users.noreply.github.com>
2025-12-11 14:07:46 -0600

    Remove unneeded page route

--- Frontend/Elegant-Notes/src/router/routes.js
+++ Frontend/Elegant-Notes/src/router/routes.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router"
 
 import PageView from "@/components/PageView.vue"
 import PageContent from "@/components/PageContent.vue"
-import NewPageDialog from "@/components/NewPageDialog.vue"
 
 const routes = [
     {
@@ -14,11 +13,6 @@ const routes = [
         path: '/page-content',
         name: 'PageContent',
         component: PageContent
-    },
-    {
-        path: '/new-page',
-        name: 'NewPage',
-        component: NewPageDialog
     },
 
     {
